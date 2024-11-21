ef847671c86262bcf75b751cf449e03b33eeb26b
Israel jimenez <sylcredjimenez19@gmail.com>
2024-11-21 12:12:26 -0400

    Agregar archivos de proyecto.

---
+++ LoadDwh.Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace LoadDwh.Data
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ LoadDwh.WorkerService/Program.cs
@@ -0,0 +1,10 @@
+using LoadDwh.WorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
